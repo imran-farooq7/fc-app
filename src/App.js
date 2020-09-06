1bf78261d1e3c9501ab5d1df65977bdef97d5d84
unknown <you@example.com>
2020-09-06 05:35:41 +0500

    added header component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import Homepage from "./pages/Homepage/Homepage";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/Homepage/shopPage/ShopPage";
+import Header from "./Components/header/Header";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/shop" component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
