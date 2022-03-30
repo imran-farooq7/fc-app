93445b415c42fcbc103be8ed01409bfabb2de2c4
imran farooq <imran_farooq7@hotmail.com>
2022-03-30 17:55:39 +0500

    created project with CRA and added homepage file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React from "react";
 import "./App.css";
-import Homepage from "./pages/Homepage/Homepage";
-import { Switch, Route } from "react-router-dom";
-import ShopPage from "./pages/Homepage/shopPage/ShopPage";
-import Header from "./Components/header/Header";
-import SignIn from "./pages/sign-in-and-sign-up/SignIn";
+import HomePage from "./HomePage";
+import "./HomePage.scss";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={SignIn} />
-      </Switch>
-    </div>
-  );
+	return (
+		<div>
+			<HomePage />
+		</div>
+	);
 }
 
 export default App;
